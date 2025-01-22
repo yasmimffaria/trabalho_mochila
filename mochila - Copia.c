39123e2817cecfae0cbbe91f70ebde692e4e820f
JuliaNakano <jumartinsnakano@gmail.com>
2025-01-22 14:58:35 -0300

    Foi realizado as funções que calculam o peso total e o beneficio do subconjunto

--- mochila - Copia.c
+++ mochila - Copia.c
@@ -16,11 +16,10 @@ typedef struct tipoMochila {
 
 //Função de teste para imprimir um vetor 
 void imprimirVet(int Vet[], int n) {
-    printf("\n");
     printf("####Vetor##### \n");
-
     for (int i = 0; i < n; i++) {
         printf("%d", Vet[i]);
+        
     }
     printf("\n");
 
@@ -29,7 +28,8 @@ void imprimirVet(int Vet[], int n) {
 
 //Tranforma um numero x em um vetor binario onde  quantidade de posições é equivalente a quantidade de bits 
 // do numero.
-void contarBinario(int numero, int n, int* Vet) {
+void contarBinario(int numero, int n) {
+    int Vet[n];
     for (int i = n - 1; i >= 0; i--) {
        int bit = (numero / (int)pow(2, i)) % 2; // 1/4 = 0 0 mod 2 = 0  0 mod 2
         //printf("%d ---\n", bit);
@@ -41,30 +41,6 @@ void contarBinario(int numero, int n, int* Vet) {
 
 }
 
-//calcula o peso de um subconjunto e retorna o peso total 
-float calcula_peso(tipoMochila mochila, int vetor[MAXTAM]){
-    float peso_total=0;
-    for  (int i =0; i<mochila.N; i++){
-
-        if(vetor[i]==1){
-            peso_total+= mochila.peso[i];
-        }
-    }
-    
-    return peso_total;
-}
-
-//calcula o beneficio de um subconjunto e retorna o beneficio total 
-float calcula_beneficio(tipoMochila mochila, int vetor[MAXTAM]){
-    float beneficio_total=0;
-    for  (int i =0; i<mochila.N; i++){
-
-        if(vetor[i]==1){
-            beneficio_total+= mochila.beneficio[i];
-        }
-    }
-    return beneficio_total;
-}
 
 tipoMochila criar_mochila(char *StrEntrada) {
     // Funcao cria uma nova mochila, lendo do arquivo e trazendo todos os dados do txt para a memoria
@@ -159,29 +135,13 @@ int n;
     //se eu tiver 3 itens, vou ter 8 combinações possiveis
     int limite = pow(2,n); 
 
-    
-
 
     //exibe todos os números de 0 até 2^n - 1 em binário, ou seja se tiver 4 itens vai pecorrer de 0 a 15
-    int sub_conjunto[n];
-    printf("\n");
     for (int i = 0; i < limite; i++) {
-       
-       contarBinario(i,n,sub_conjunto);
-       
         
-
-        printf("PESO: %.2f \n",calcula_peso(Mochila, sub_conjunto) );
-        printf("BENEFICIO: %.2f \n",calcula_beneficio(Mochila, sub_conjunto) );
+        contarBinario(i, n); // exemplo 1,4
         printf("\n");
-
-
-
-
-
     }
-    printf("\n");
-
 
 
     return 0;
