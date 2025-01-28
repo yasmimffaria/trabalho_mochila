10c26c9261179660cb92ef3992725e262cec8264
Victor Luis <victor.rbread@gmail.com>
2025-01-28 20:37:54 -0300

    Adicionei o resto dos parametros, agora precisa especificar o metodo, arquivo de entrada e arquyivo de saida sempre que for rodar o arquivo.
    Para o metodo, E pra enumeração e H pra heuristica, mas qualquer outra coisa vai rodar os dois, enumeração primeiro, oq facilita os testes.
    
    Separei a funcao da heuristica da funcao que ordena os vetor com os indices das razoes.
    
    Criei uma funcao que printa um subconjunto especifico, com seu beneficio e seu peso total.
    
    Também, organizei o codigo do main, separando em partes diferentes cada segmento do codigo.

--- mochila.c
+++ mochila.c
@@ -12,13 +12,12 @@ typedef struct tipoMochila
     char nome[MAXTAM][256];  // nome do item
     float peso[MAXTAM];      // peso do item
     float beneficio[MAXTAM]; // beneficio do item
-    float razao[MAXTAM];     // razão entre o beneficio e o peso
+    float razao[MAXTAM];     // razao entre o beneficio e o peso
 } tipoMochila;
 
 // Transforma o vetor "Vet" em uma representacao binaria do valor "numero"
 // EX: 5 => [1,0,1]
-void contarBinario(int numero, int n, int Vet[])
-{
+void contarBinario(int numero, int n, int Vet[]){
     for (int i = n - 1; i >= 0; i--)
     {
         int bit = (numero / (int)pow(2, i)) % 2; // 1/4 = 0 0 mod 2 = 0  0 mod 2
@@ -28,8 +27,7 @@ void contarBinario(int numero, int n, int Vet[])
 }
 
 // calcula o peso de um subconjunto e retorna o peso total
-float calcula_peso(tipoMochila mochila, int vetor[MAXTAM])
-{
+float calcula_peso(tipoMochila mochila, int vetor[]){
     float peso_total = 0;
     for (int i = 0; i < mochila.N; i++)
     {
@@ -42,8 +40,7 @@ float calcula_peso(tipoMochila mochila, int vetor[MAXTAM])
 }
 
 // calcula o beneficio de um subconjunto e retorna o beneficio total
-float calcula_beneficio(tipoMochila mochila, int vetor[MAXTAM])
-{
+float calcula_beneficio(tipoMochila mochila, int vetor[]){
     float beneficio_total = 0;
     for (int i = 0; i < mochila.N; i++)
     {
@@ -55,9 +52,8 @@ float calcula_beneficio(tipoMochila mochila, int vetor[MAXTAM])
     return beneficio_total;
 }
 
-// Traz a mochila de um arquivo, e guarda na struct que é usada de retorno
-tipoMochila criar_mochila(char *StrEntrada)
-{
+// Traz a mochila de um arquivo, e guarda na struct que e usada de retorno
+tipoMochila criar_mochila(char *StrEntrada){
     // declaracao de variaveis
     char Controle;       // le o caracter e decide o que faz
     char auxFinalLinha;  // auxiliar para ir para próxima linha
@@ -102,7 +98,7 @@ tipoMochila criar_mochila(char *StrEntrada)
 
         // # - Ignorar linha
         case '#':
-            /* Lê o restante da linha, até chegar no final de linha '\n' */
+            /* Le o restante da linha, ate chegar no final de linha '\n' */
             do
             {
                 fscanf(arquivo, "%c", &auxFinalLinha);
@@ -129,8 +125,7 @@ tipoMochila criar_mochila(char *StrEntrada)
 }
 
 // Printa um Vetor
-void imprimirVet(int Vet[], int n)
-{
+void imprimirVet(int Vet[], int n){
     printf("\n");
     printf("####Vetor##### \n");
 
@@ -142,8 +137,7 @@ void imprimirVet(int Vet[], int n)
 }
 
 // Printa todos os itens e detalhes da mochila
-void imprimirMochila(tipoMochila mochila)
-{
+void imprimirMochila(tipoMochila mochila){
     // printa o header
     printf("\n ======================================================================================");
     printf("\n |                                   M O C H I L A                                    |");
@@ -165,8 +159,7 @@ void imprimirMochila(tipoMochila mochila)
 }
 
 // Printa todos os subconjuntos do conjunto potencia
-void imprimirPotencia(tipoMochila mochila)
-{
+void imprimirPotencia(tipoMochila mochila){
     // cria um vetor pra representar o subconjunto
     int subconjunto[mochila.N];
 
@@ -185,13 +178,12 @@ void imprimirPotencia(tipoMochila mochila)
 
         // cria uma versao do mapa de bits que e na vdd uma string
         //  assim, e mais facil de printar
-        int tam = sizeof(subconjunto) / sizeof(subconjunto[0]);
-        char subString[tam + 1]; // adiciona mais um pra ter o \0 q strings precisam
-        for (int j = 0; j < tam; j++)
+        char subString[mochila.N + 1]; // adiciona mais um pra ter o \0 q strings precisam
+        for (int j = 0; j < mochila.N; j++)
         {
             subString[j] = subconjunto[j] ? '1' : '0'; // se tiver um 1, troca pelo caractere '1', se n, coloca '0'
         }
-        subString[tam] = '\0'; // coloca o final da string
+        subString[mochila.N] = '\0'; // coloca o final da string
 
         // agora printa a linha corretamente
         printf("\n | %-40s | %-18g | %-18g |", subString, calcula_peso(mochila, subconjunto), calcula_beneficio(mochila, subconjunto));
@@ -201,68 +193,57 @@ void imprimirPotencia(tipoMochila mochila)
     printf("\n ======================================================================================\n");
 }
 
-void enumeracao(tipoMochila mochila, int vetOtimo[], float *peso_otimo, float *beneficio_otimo)
-{
-    *beneficio_otimo = 0;
-    *peso_otimo = 0;
+// Printa um subconjunto, mostrando o mapa de bits, o beneficio e o peso total dele.
+void imprimirSubconjunto(tipoMochila mochila, int vetor[]){
+    printf("\n ======================================================================================");
+    printf("\n | Mapa de bits: ");
+    for (int i = 0; i < mochila.N; i++){
+        printf("%d ",vetor[i]);
+    }
+    printf("\n | Peso total:      %g", calcula_peso(mochila, vetor));
+    printf("\n | Beneficio total: %g", calcula_beneficio(mochila, vetor));
+    printf("\n ======================================================================================");
+}
+
+// Metodo de enumeracao
+void enumeracao(tipoMochila mochila, int vetOtimo[]){
     vetOtimo[0];
     int vet_auxiliar[mochila.N];
 
-    for (int i = 0; i < pow(2, mochila.N); i++)
-    {
+    for (int i = 0; i < pow(2, mochila.N); i++){
         contarBinario(i, mochila.N, vet_auxiliar);
 
-        if (calcula_peso(mochila, vet_auxiliar) <= mochila.K)
-        {
-            if (calcula_beneficio(mochila, vet_auxiliar) > *beneficio_otimo)
-            {
-                for (int j = 0; j < mochila.N; j++)
-                {
-                    vetOtimo[j] = vet_auxiliar[j];
-                }
-
-                *beneficio_otimo = calcula_beneficio(mochila, vet_auxiliar);
-                *peso_otimo = calcula_peso(mochila, vet_auxiliar);
+        if ((calcula_peso(mochila, vet_auxiliar) <= mochila.K) && (calcula_beneficio(mochila, vet_auxiliar) > calcula_beneficio(mochila, vetOtimo))){
+            for (int j = 0; j < mochila.N; j++){
+                vetOtimo[j] = vet_auxiliar[j];
             }
         }
     }
-
-    printf("\n");
-    printf("Este é o melhor subconjunto pelo metodo da Enumeracao");
-    printf("\n");
-    for (int i = 0; i < mochila.N; i++)
-    {
-        printf("%d ", vetOtimo[i]);
-    }
-    printf("\n");
-    printf("Tendo o Peso : %g", *peso_otimo);
-    printf("\n");
-    printf("Tendo o beneficio :%g ", *beneficio_otimo);
 }
 
-float heuristica(tipoMochila mochila, int vetOtimo[])
-{
-    int vetorIndices[mochila.N];
-    // Iniciando vetor com zeros
-    for (int i = 0; i < mochila.N; i++)
-    {
+// Ordena os itens baseado nas suas razoes, e retorna seus indices em um vetor
+void ordenaIndicesRazoes(tipoMochila mochila, int vetorIndices[]){
+    //comeca zerado
+    for (int i = 0; i < mochila.N; i++){
         vetorIndices[i] = 0;
     }
-    int cont = 0;
 
     // vetor para controlar se aquele item ja foi selecionado
     int selecionado[mochila.N];
-    for (int i = 0; i < mochila.N; i++)
-    {
+    //comeca zerado
+    for (int i = 0; i < mochila.N; i++){
         selecionado[i] = 0;
     }
 
-    do
-    {
-        int indiceMaiorRazao = -1;
-        float maiorRazao = -1;
+    //contador de itens, pra saber quando ja ordenou todos.
+    int cont = 0;
 
-        // encontra o indice com maior razao q n foi selecionado
+    do{
+        // maior razao encontrada, e o indice dessa maior razao
+        float maiorRazao = -1;
+        int indiceMaiorRazao = -1;
+
+        // percorre os itens e encontra o indice com maior razao q n foi selecionado
         for (int i = 0; i < mochila.N; i++)
         {
             if (!selecionado[i] && mochila.razao[i] > maiorRazao)
@@ -275,83 +256,85 @@ float heuristica(tipoMochila mochila, int vetOtimo[])
         // coloca o indice encontrado como selecionado no vetor
         selecionado[indiceMaiorRazao] = 1;
 
+        // atribui o indice no vetor dos indices, e adiciona um no contador
         vetorIndices[cont] = indiceMaiorRazao;
         cont++;
     } while (cont < mochila.N);
+}
+
+//Metodo de heuristica
+void heuristica(tipoMochila mochila, int vetOtimo[]){
+    // vetor que vai guardar os indices dos itens ordenado pela razao
+    int vetorIndices[mochila.N];
+    ordenaIndicesRazoes(mochila, vetorIndices);
 
     // Acessando cada indice e somando beneficio e peso
     float somadorBeneficio = 0;
     float somadorPeso = 0;
-    int vetorHeuristica[mochila.N]; // Vetor para os itens da heuristica
 
     // Zerando vetor para evitar lixo de memoria
     for (int i = 0; i < mochila.N; i++)
-        vetorHeuristica[i] = 0;
+        vetOtimo[i] = 0;
 
     // Se item couber na mochila, coloque-o
-    for (int i = 0; i < mochila.N; i++)
-    {
+    for (int i = 0; i < mochila.N; i++){
         if (mochila.K >= (somadorPeso + mochila.peso[vetorIndices[i]]))
         {
-            vetorHeuristica[vetorIndices[i]] = 1;
+            vetOtimo[vetorIndices[i]] = 1;
             somadorPeso += mochila.peso[vetorIndices[i]];
             somadorBeneficio += mochila.beneficio[vetorIndices[i]];
         }
     }
 
-    printf("\nEste e o melhor subconjunto pelo metodo da heuristica:\n");
-    for (int j = 0; j < mochila.N; j++)
-    {
-        printf("%d ", vetorHeuristica[j]);
-    }
-
-    printf("\nPeso: %.0f", calcula_peso(mochila, vetorHeuristica));
-    printf("\nBeneficio: %.1f", calcula_beneficio(mochila, vetorHeuristica));
-    return calcula_beneficio(mochila, vetorHeuristica);
 }
 
-int main(int argc, char **argv)
-{
-    // definição de variaveis
-    char *StrEntrada;
-    tipoMochila Mochila;
+int main(int argc, char **argv){
+    // ===================================================
+    // ARGUMENTOS
 
     // confere se o num de argumentos ta certo
-    if (argc != 2)
+    if (argc != 4)
     {
         printf("\nErro de Sintaxe\n");
-        printf("Usar: ./solver-mochila.bin <entrada>\n\n");
+        printf("Usar: ./mochila.bin <metodo> <entrada> <saida>\n\n");
         return 1;
     }
 
-    // se os argumentos estao certo, recebe eles
-    StrEntrada = argv[1];
+    // se os argumentos estao certos, recebe eles
+    char metodo = argv[1][0];   // metodo: E - Enumeracao & H - Heuristica // qlqr outra coisa pra usar os dois
+    char *StrEntrada = argv[2]; // nome do arquivo de entrada
+    char *StrSaida = argv[3];   // nome do arquivo de saida
 
-    // roda a funcao que pega a mochila do arquivo e coloca na variavel Mochila
-    Mochila = criar_mochila(StrEntrada);
+    // ===================================================
+    // MOCHILA
 
-    //=======TESTE=======
+    tipoMochila Mochila = criar_mochila(StrEntrada); // mochila que vai guardar todos os detalhes
+    int vetOtimo[Mochila.N];                         // mapa de bits do melhor subconjunto encontrado
+
+    // ===================================================
+    // PRINTS DE TESTE
 
     // mostra a mochila
     imprimirMochila(Mochila);
 
     // mostra os conjuntos
-    //imprimirPotencia(Mochila);
+    imprimirPotencia(Mochila);
 
-    int vetOtimo[Mochila.N];
-    float peso_otimo;
-    float beneficio_otimo;
-    enumeracao(Mochila, vetOtimo, &peso_otimo, &beneficio_otimo);
+    // ===================================================
+    // RESOLUCAO DO PROBLEMA
 
-    printf("\n\n");
+    if ( metodo == 'E' ){
+        enumeracao(Mochila, vetOtimo);
+        imprimirSubconjunto(Mochila, vetOtimo);
+    } else if ( metodo == 'H' ){
+        heuristica(Mochila, vetOtimo);
+        imprimirSubconjunto(Mochila, vetOtimo);
+    } else {
+        enumeracao(Mochila, vetOtimo);
+        imprimirSubconjunto(Mochila, vetOtimo);
+        heuristica(Mochila, vetOtimo);
+        imprimirSubconjunto(Mochila, vetOtimo);
+    }
 
-    // Retorna o beneficio da heuristica
-    float beneficioHeuristica;
-    beneficioHeuristica = heuristica(Mochila, vetOtimo);
-
-    // Gap
-    printf("\n\nGap(%%) = (%.1f - %.1f)/%.1f = %.0f%%", beneficio_otimo, beneficioHeuristica, beneficio_otimo, ((beneficio_otimo - beneficioHeuristica) / beneficio_otimo) * 100);
-
-    //=====TESTE-FIM=====
     return 0;
 }
